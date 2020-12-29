d64d156617adab9518727a58bc84b3cb1d077db7
Diana Zambrano <damari01zambrano@gmail.com>
2020-12-29 16:21:19 -0500

    Pantalla retiros funcionando todos los campos

---
+++ BANCO/CNTAS/ADMIN/forms/T_CNTASGVQQWGCD_328/events/executeCommand/VA_VABUTTONCBQVSQM_606860_Cbk.js
@@ -0,0 +1,5 @@
+
+//Start signature to Callback event to VA_VABUTTONCBQVSQM_606860
+task.executeCommandCallback.VA_VABUTTONCBQVSQM_606860 = function(entities, executeCommandCallbackEventArgs) {
+     //here your code
+};
---
+++ BANCO/CNTAS/ADMIN/forms/T_CNTASGVQQWGCD_328/events/executeCommand/VA_VABUTTONROHFQQZ_817860_Cbk.js
@@ -0,0 +1,5 @@
+
+//Start signature to Callback event to VA_VABUTTONROHFQQZ_817860
+task.executeCommandCallback.VA_VABUTTONROHFQQZ_817860 = function(entities, executeCommandCallbackEventArgs) {
+     //here your code
+};
