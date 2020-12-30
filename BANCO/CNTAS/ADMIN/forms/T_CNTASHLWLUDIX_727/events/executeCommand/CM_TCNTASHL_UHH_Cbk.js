4c4804bfb67d1374105042467e9d4da7326494fa
Paul Guaman <guaman.bryan.paul@gmail.com>
2020-12-30 14:45:02 -0500

    modal

---
+++ BANCO/CNTAS/ADMIN/forms/T_CNTASHLWLUDIX_727/events/executeCommand/CM_TCNTASHL_UHH_Cbk.js
@@ -0,0 +1,6 @@
+
+//Start signature to Callback event to CM_TCNTASHL_UHH
+task.executeCommandCallback.CM_TCNTASHL_UHH = function(entities, executeCommandCallbackEventArgs) {
+     //here your code
+       executeCommandCallbackEventArgs.commons.api.vc.closeModal({});
+};
